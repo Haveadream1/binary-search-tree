cd48e589aeeb0761fbfd4d53d4bb52271929418f
Haveadream1 <260602david@gmail.com>
2025-04-02 07:54:08 +0200

    feat: add main test

---
+++ main.js
@@ -0,0 +1,29 @@
+import Tree from "./tree.js";
+
+const tree = new Tree();
+
+// tree.buildTree(array);
+
+// tree.prettyPrint();
+
+// tree.insert(value);
+
+// tree.deleteItem(value);
+
+// tree.find(value);
+
+// tree.levelOrder(callback);
+
+// tree.inOrder(callback);
+
+// tree.preOrder(callback);
+
+// tree.postOrder(callback);
+
+// tree.height(node);
+
+// tree.depth(node);
+
+// tree.isBalanced();
+
+// tree.rebalance();
