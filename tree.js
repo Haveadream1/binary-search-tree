13f50b3ded50ec9a5b9d3e272c81662f64d1c482
Haveadream1 <260602david@gmail.com>
2025-04-04 17:48:26 +0200

    feat: add methods

--- tree.js
+++ tree.js
@@ -73,6 +73,7 @@ export default class BinarySearchTree {
         }
         return currentNode;
     }
+
     levelOrder() {
         // traverse the tree in breadth-first level order
         const tree = [];
@@ -136,26 +137,49 @@ export default class BinarySearchTree {
 
     height(node) {
         // returns the given node’s height
+        // number of edges on the longest path
         if (!node) {
-            return 0;
+            return -1;
+        } else {
+            const leftHeight = this.height(node.left); // Recursivly calculate the height of left subtree
+            const rightHeight = this.height(node.right); 
+
+            return Math.max(leftHeight, rightHeight) + 1; // Compare and take the longest, +1 represents the current node
         }
-
-        const leftHeight = this.height(node.left);
-        const rightHeight = this.height(node.right); 
-
-        return Math.max(leftHeight, rightHeight) + 1;
     }
 
     depth(node) {
         // returns the given node’s depth
         // the number of edges in the path from a given node to the tree’s root node
+        function recursiveDepth(node, currentDepth) { // We need to know the depth of the parent
+            if (!node) {
+                return currentDepth;
+            } else {
+                return recursiveDepth(node.parent, currentDepth + 1);
+            }
+        }
+        return recursiveDepth(node, 0); // Init
     }
 
-    isBalanced() {
-        // balanced tree is one where the difference between heights of the left subtree and the right subtree of every node is not more than 1
+    isBalanced(node) {
+        // balanced tree is one where the difference between heights of the left subtree and 
+        // the right subtree of every node is not more than 1
+        if (!node) {
+            return true;
+        }
+
+        const leftHeight = this.height(node.left);
+        const rightHeight = this.height(node.right);
+
+        if (Math.abs(leftHeight - rightHeight) > 1) { // If the absolute difference is more than 1 then its not balanced
+            return false;
+        }
+        return this.isBalanced(node.left) && this.isBalanced(node.right); // Recursivly call left and right node
     }
 
-    rebalance() {
+    rebalance(node) {
+        if (!this.isBalanced(node)) {
 
+        }
     }
 }
